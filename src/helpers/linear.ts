0522a3d6820cbe3f8b5c458ee3d3324706b2629d
8BitJonny <christiansen.timon@gmail.com>
2022-10-20 12:45:06 +0200

    (core): match linearBranch caseInsensitive

--- src/helpers/linear.ts
+++ src/helpers/linear.ts
@@ -1,5 +1,5 @@
 function IsLinearBranch(branch: string) : [string, string, boolean] {
-    const matches = branch.matchAll(/^([a-z]+-\d+)-([\da-z-]+)$/g)
+    const matches = branch.matchAll(/^([a-zA-Z]+-\d+)-([\da-zA-Z-]+)$/g)
     const match = matches.next();
     
     if (match.done) return ["", "", false];
